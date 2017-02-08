bb8699f42260ced58dbc3a48643144aea929cafa
Disa <justdisa@gmail.com>
2017-02-08 12:40:51 -0800

    basic ideas in my head, if not on screen

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function project (name, version, webaddress, category, description, image) {
+  this.name = name;
+  this.version = version;
+  this.webaddress = webaddress;
+  this.category = category;
+  this.description = description;
+}
