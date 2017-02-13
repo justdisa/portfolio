30637a53ff58c92cea64d2c1282ee07a33aa27db
Disa <justdisa@gmail.com>
2017-02-13 12:44:39 -0800

    some javascript

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,1 +1,67 @@
 'use strict';
+
+var projectView = {};
+
+projectView.populateFilters = function () {
+  $('project').each(function() {
+    if(!$(this).hasClass('template')) {
+      var val = $(this).find('address a').text();//not sure about this line//
+      var optionTag = `<option value="${val}">${val}</option>`;
+
+      if($(`#collaborator-filter option[value="${val}"]`).length === 0) {
+        $('collaborator-filter').append(optionTag);
+      }
+
+      val = $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+
+      if($`#category-filter option[value="${val}"]`).length === 0 {
+        $('category-filter').append(optionTag);
+      }
+    }
+  })
+};
+
+projectView.handleCollaboratorFilter = function() {
+  $('#collaborator-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $(`project[data-collatorator="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('template').hide();
+    }
+  $('#category-filter').val('');
+  });
+};
+
+projectView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $(`project[data-category="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('template').hide();
+    }
+  $('#collaborator-filter').val('');
+  });
+};
+
+projectView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function(e) {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  })
+
+  $('.main-nav .tab:first').click();
+};
+
+projectView.setTeasers = function() {
+  $(`.project-body' *:nth-of-type(n+2)`).hide();
+  $('#projects').on('click', 'a.read-on', function() {
+    e.preventDefault();
+  $(this).parent().find('*').fadeIn();
+  $(this).hide();
+});
+};
